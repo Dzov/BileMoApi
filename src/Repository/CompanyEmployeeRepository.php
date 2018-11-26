0279ef368326384231d5a44e5752bcddcd945e8c
Dzov <amelie.haladjian@gmail.com>
2018-11-26 19:06:08 +0100

    refacto(*): clean codeg

--- src/Repository/CompanyEmployeeRepository.php
+++ src/Repository/CompanyEmployeeRepository.php
@@ -18,33 +18,4 @@ class CompanyEmployeeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, CompanyEmployee::class);
     }
-
-    // /**
-    //  * @return CompanyEmployee[] Returns an array of CompanyEmployee objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?CompanyEmployee
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
