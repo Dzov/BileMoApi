396b604d9b344f84e4fb2abd1502eb268057b4c2
Dzov <amelie.haladjian@gmail.com>
2018-11-26 16:00:39 +0100

    feat(security): add and configure authenticator

---
+++ src/Security/ApiAuthenticator.php
@@ -0,0 +1,68 @@
+<?php
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+class ApiAuthenticator extends AbstractGuardAuthenticator
+{
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = ['message' => 'Authentication Required'];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supports(Request $request)
+    {
+        return true;
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return ['apiKey' => $request->headers->get('apiKey'), 'apiPassword' => $request->headers->get('apiPassword')];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiKey = $credentials['apiKey'];
+
+        if (null === $apiKey) {
+            return null;
+        }
+
+        $userProvider->loadUserByUsername($apiKey);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
